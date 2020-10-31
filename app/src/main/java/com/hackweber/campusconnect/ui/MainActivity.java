c236f549f6320ee3e996cddb7512c9d14b75d370
harshit <harshitagr98@gmail.com>
2020-11-01 03:45:52 +0530

    Bottom navigation switch case fix

--- app/src/main/java/com/hackweber/campusconnect/ui/MainActivity.java
+++ app/src/main/java/com/hackweber/campusconnect/ui/MainActivity.java
@@ -60,18 +60,22 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.home:
                         Intent i=new Intent(MainActivity.this,MainActivity.class);
                         startActivity(i);
+                        finish();
                         break;
                     case R.id.lost_found:
                         Intent i2=new Intent(MainActivity.this, LostAndFound.class);
                         startActivity(i2);
+                        finish();
                         break;
                     case R.id.report:
                         Intent i3=new Intent(MainActivity.this,MainActivity.class);
                         startActivity(i3);
+                        finish();
                         break;
                     case R.id.profile:
                         Intent i4=new Intent(MainActivity.this,MainActivity.class);
                         startActivity(i4);
+                        finish();
                         break;
 
                 }
