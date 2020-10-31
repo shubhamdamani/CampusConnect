53369dbf5dec3515610c7d1bc29218710025944a
Tanish <tanishy0702@gmail.com>
2020-10-31 18:03:45 +0530

    Fetched list items done

--- app/src/main/java/com/hackweber/campusconnect/LostAndFound/ItemAdapter.java
+++ app/src/main/java/com/hackweber/campusconnect/LostAndFound/ItemAdapter.java
@@ -1,4 +1,76 @@
 package com.hackweber.campusconnect.LostAndFound;
 
-public class ItemAdapter {
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hackweber.campusconnect.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder>{
+
+    private List<ItemInfo> itemList;
+    private Context context;
+
+    public ItemAdapter(List<ItemInfo> itemList, Context context) {
+        this.itemList = itemList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.raw_lost_and_found_item,parent,false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        ItemInfo item = itemList.get(position);
+        Picasso.get().load(Uri.parse(item.getItemUri())).placeholder(R.mipmap.ic_launcher).into(holder.itemImage);
+        holder.itemName.setText(item.getItemName());
+        holder.itemPlace.setText(item.getItemPlace());
+        holder.itemDate.setText(item.getItemDate());
+    }
+
+    public void setData(List<ItemInfo> itemList){
+        this.itemList = itemList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView itemImage;
+        public TextView itemName,itemPlace,itemDate;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemImage = itemView.findViewById(R.id.raw_lost_and_found_item_image);
+            itemName = itemView.findViewById(R.id.raw_lost_and_found_item_itemName);
+            itemPlace = itemView.findViewById(R.id.raw_lost_and_found_item_itemPlace);
+            itemDate = itemView.findViewById(R.id.raw_lost_and_found_item_itemDate);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    
+                }
+            });
+
+        }
+    }
+
 }
--- app/src/main/java/com/hackweber/campusconnect/LostAndFound/lostAndFound.java
+++ app/src/main/java/com/hackweber/campusconnect/LostAndFound/lostAndFound.java
@@ -1,5 +1,6 @@
 package com.hackweber.campusconnect.LostAndFound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,17 +11,28 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hackweber.campusconnect.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class lostAndFound extends AppCompatActivity {
 
     private TextView lostItems,foundItems;
     private RecyclerView recyclerView;
+    private ItemAdapter adapter;
     private LinearLayoutManager layoutManager;
+    private List<ItemInfo> itemList;
     private FloatingActionButton addItems;
     private int item_type=0;
     //0 for lost 1 for found
     private String item_Category="LostItems";
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +41,14 @@ public class lostAndFound extends AppCompatActivity {
 
         init();
 
+        fetchItems(item_Category);
+
         lostItems.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 item_type=0;
                 item_Category="LostItems";
+                fetchItems(item_Category);
             }
         });
 
@@ -42,6 +57,7 @@ public class lostAndFound extends AppCompatActivity {
             public void onClick(View view) {
                 item_type=1;
                 item_Category="FoundItems";
+                fetchItems(item_Category);
             }
         });
 
@@ -55,12 +71,36 @@ public class lostAndFound extends AppCompatActivity {
         });
     }
 
+    private void fetchItems(final String item_category) {
+        itemList.clear();
+        databaseReference.child("LostAndFoundItems").child(item_category).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    ItemInfo item = ds.getValue(ItemInfo.class);
+                    itemList.add(item);
+                }
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
     private void init() {
+        itemList = new ArrayList<>();
         layoutManager = new LinearLayoutManager(getApplicationContext());
         lostItems = findViewById(R.id.lost_items_list);
         foundItems = findViewById(R.id.found_items_list);
         recyclerView = findViewById(R.id.lost_and_found_recylerView);
         recyclerView.setHasFixedSize(true);
+        adapter = new ItemAdapter(itemList,getApplicationContext());
         addItems = findViewById(R.id.lost_and_found_add_items);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 }
